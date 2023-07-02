947ce849458c4337b91d2e927d6ade606f7e54ca
Eddie <eddie@edwinfairchild.com>
2023-07-01 22:47:20 -0500

    clean up extension.ts file

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,58 +1,66 @@
 import * as vscode from 'vscode';
 
+function createWebviewPanel(context: vscode.ExtensionContext) {
+    const panel = vscode.window.createWebviewPanel(
+        'maxblugenWebview',
+        'MaxBluGen',
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true,
+            localResourceRoots: [vscode.Uri.file(context.extensionPath)]
+        }
+    );
+
+    return panel;
+}
+
+async function setWebviewHtml(panel: vscode.WebviewPanel, context: vscode.ExtensionContext) {
+    const htmlPath = vscode.Uri.joinPath(context.extensionUri, 'webview.html');
+
+    const data = await vscode.workspace.fs.readFile(htmlPath);
+    const dataAsString = new TextDecoder().decode(data);
+
+    const htmlUrl = panel.webview.asWebviewUri(htmlPath);
+    let htmlContent = dataAsString.replace(/(href|src)="\.\//g, `$1="${htmlUrl.toString()}/`);
+    panel.webview.html = htmlContent;
+}
+
+function handleWebviewMessages(panel: vscode.WebviewPanel, context: vscode.ExtensionContext) {
+    panel.webview.onDidReceiveMessage(
+        async message => {
+            switch (message.type) {
+                case 'buttonClicked':
+                    vscode.window.showInformationMessage(message.text);
+                    break;
+                case 'writeToFile':
+                    const filePath = vscode.Uri.joinPath(context.extensionUri, 'myFile.txt');
+                    const writeData = new TextEncoder().encode('Hello, file!');
+                    await vscode.workspace.fs.writeFile(filePath, writeData);
+                    vscode.window.showInformationMessage('Written to file!');
+                    break;
+            }
+        },
+        undefined,
+        context.subscriptions
+    );
+}
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "maxblugen" is now active in the web extension host!');
 
-    let disposable = vscode.commands.registerCommand('maxblugen.helloWorld', async () => { 
-        const panel = vscode.window.createWebviewPanel(
-            'maxblugenWebview',
-            'MaxBluGen',
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true, 
-                localResourceRoots: [vscode.Uri.file(context.extensionPath)]
-            }
-        );
-
-        const htmlPath = vscode.Uri.joinPath(context.extensionUri, 'webview.html');
-
-        const data = await vscode.workspace.fs.readFile(htmlPath); 
-        const dataAsString = new TextDecoder().decode(data); 
-
-        const htmlUrl = panel.webview.asWebviewUri(htmlPath);
-        let htmlContent = dataAsString.replace(/(href|src)="\.\//g, `$1="${htmlUrl.toString()}/`);
-        panel.webview.html = htmlContent;
-
-        // Listen for messages from the webview
-        panel.webview.onDidReceiveMessage(
-            async message => {
-				vscode.window.showInformationMessage(message.text);
-                switch (message.type) {
-                    case 'buttonClicked':
-                        vscode.window.showInformationMessage(message.text);
-                        break;
-					case 'writeToFile':
-						const filePath = vscode.Uri.joinPath(context.extensionUri, 'myFile.txt');
-						const writeData = new TextEncoder().encode('Hello, file!');
-						await vscode.workspace.fs.writeFile(filePath, writeData);
-						vscode.window.showInformationMessage('Written to file!');
-						break;
-						
-                }
-            },
-            undefined,
-            context.subscriptions
-        );
+    let disposable = vscode.commands.registerCommand('maxblugen.helloWorld', async () => {
+        const panel = createWebviewPanel(context);
+        await setWebviewHtml(panel, context);
+        handleWebviewMessages(panel, context);
     });
 
-	let writeDisposable = vscode.commands.registerCommand('maxblugen.writeToFile', async () => { 
+    let writeDisposable = vscode.commands.registerCommand('maxblugen.writeToFile', async () => {
         const filePath = vscode.Uri.joinPath(context.extensionUri, 'myFile.txt');
         const writeData = new TextEncoder().encode('Hello, file!');
         await vscode.workspace.fs.writeFile(filePath, writeData);
         vscode.window.showInformationMessage('Written to file!');
     });
-    context.subscriptions.push(disposable,writeDisposable);
+    context.subscriptions.push(disposable, writeDisposable);
 }
 
 export function deactivate() {}
